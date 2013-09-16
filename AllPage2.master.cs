2989d0e5173f578569b40836be25415d91e6959f
unknown <Administrator@FREESKYC-E2D835.(none)>
2013-09-16 13:49:22 +0800

    deleted:    "Images/\346\234\252\345\221\275\345\220\215-2.gif"

---
+++ AllPage2.master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class AllPage : System.Web.UI.MasterPage
+{
+    public LibWeb mylib = new LibWeb();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        mylib.WebDataOpen();
+        mylib.GetInfo();
+        mylib.WebDataClose();
+        Page.Title = mylib.Title;
+        HtmlHead pagehead = (HtmlHead)Page.Header;//首先把Page.Header转换成HtmlHead
+        HtmlMeta PageMeta1 = new HtmlMeta();
+        HtmlMeta PageMeta2 = new HtmlMeta();
+        PageMeta1.Name = "keywords";
+        PageMeta1.Content=mylib.KeyWord;
+        PageMeta2.Name = "description";
+        PageMeta2.Content = mylib.Description;
+        pagehead.Controls.Add(PageMeta1);
+        pagehead.Controls.Add(PageMeta2);
+       
+    }
+}
---
+++ research.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/AllPage2.master" AutoEventWireup="true" CodeFile="research.aspx.cs" Inherits="research" Title="Untitled Page" %>
+<%-- 在此处添加内容控件 --%>
+ <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+ wokao
+ </asp:Content>
